e6b40cc409be86e77a6a42cd898de7adcb97f718
SkyStar-K <mlnudg2000@gmail.com>
2022-04-09 14:00:50 +0900

    FEAT: add stopwatch time value input

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,21 @@ fn main() {
     let mut watch_mm: u32 = 0;
     let mut watch_ss: u32 = 0;
 
-    let mut stopwatch_hh = 0;
-    let mut stopwatch_mm = 0;
-    let mut stopwatch_ss = 0;
+    let mut stopwatch_hh = String::new();
+    let mut stopwatch_mm = String::new();
+    let mut stopwatch_ss = String::new();
+    
+    std::io::stdin().read_line(&mut stopwatch_hh).unwrap();
+    let mut stopwatch_hh: u32 = stopwatch_hh.trim().parse().unwrap();
+    std::io::stdin().read_line(&mut stopwatch_mm).unwrap();
+    let mut stopwatch_mm: u32 = stopwatch_mm.trim().parse().unwrap();
+    std::io::stdin().read_line(&mut stopwatch_ss).unwrap();
+    let mut stopwatch_ss: u32 = stopwatch_ss.trim().parse().unwrap();
 
     let handle = thread::spawn(move|| {
         loop {
             let stopwatch = NaiveTime::from_hms(stopwatch_hh, stopwatch_mm, stopwatch_ss);
-
+            
             println!("{:?} - STOPWATCH", stopwatch);
 
             if stopwatch_hh == 0 && stopwatch_mm == 0 && stopwatch_ss == 0 {
@@ -31,16 +38,16 @@ fn main() {
                 stopwatch_ss -= 1;
             }
 
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_secs(1));
         }
     });
-
+    println!("{}", stopwatch_ss);
     loop {
         let watch = NaiveTime::from_hms(watch_hh, watch_mm, watch_ss);
         
         println!("{:?} - WATCH", watch);
         
-        if watch_mm == 0 && watch_ss == 59 {
+        if watch_mm == 59 && watch_ss == 59 {
             watch_hh += 1;
             watch_mm = 0;
             watch_ss = 0;
